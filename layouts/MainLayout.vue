796d97a4663df412e6efd90180441b57cc2c48c4
David Antonio Ty <22-1-00262@vsu.edu.ph>
2024-11-09 16:34:48 +0800

    chaning login ui flag

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,69 +1,8 @@
 <template>
     <div id="MainLayout" class="w-full fixed z-50">
-        <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
-            <ul class="
-                    flex 
-                    items-center 
-                    justify-end
-                    text-xs 
-                    text-[#333333]
-                    font-light 
-                    px-2 
-                    h-10
-                    bg-[#FAFAFA] 
-                    max-w-[1200px] 
-                ">
-                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
-                    Help
-                </li>
-                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
-                    Contact Us
-                </li>
-                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
-                    About
-                </li>
-                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
-                    Buyer Protection
-                </li>
-                <li class="px-3 hover:text-[#FF4646] cursor-pointer">
-                    <Icon name="ic:sharp-install-mobile" size="17" />
-                    App
-                </li>
-                <li @mouseenter="isAccountMenu = true" @mouseleave="isAccountMenu = false"
-                    class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
-                    :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]' : 'border border-[#FAFAFA]'">
-                    <Icon name="ph:user-thin" size="17" />
-                    Account
-                    <Icon name="mdi:chevron-down" size="15" class="ml-5" />
-
-                    <div id="AccountMenu" v-if="isAccountMenu"
-                        class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b">
-                        <div v-if="!user">
-                            <div class="text-semibold text-[15px] my-4 px-3">Welcome to AliExpress!</div>
-                            <div class="flex items-center gap-1 px-3 mb-3">
-                                <NuxtLink to="/auth"
-                                    class="bg-[#0C6539] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2">
-                                    Login
-                                </NuxtLink>
-                            </div>
-                        </div>
-                        <div class="border-b" />
-                        <ul class="bg-white ">
-                            <li @click="navigateTo('/orders')" class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">
-                                My Orders
-                            </li>
-                            <li v-if="user" @click="client.auth.signOut()"
-                                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">
-                                Sign out
-                            </li>
-                        </ul>
-
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div id="MainHeader" class="flex items-center w-full bg-white">
-            <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto">
+        <div id="MainHeader"
+            class="flex items-center w-full bg-[#fafafa] shadow-[0px_4px_10px_5px_rgba(0,_0,_0,_0.25)]">
+            <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto ">
                 <NuxtLink to="/" class="min-w-[170px]">
                     <img width="170" src="/baybayani-logo.png">
                 </NuxtLink>
@@ -125,6 +64,54 @@
                         </div>
                     </button>
                 </NuxtLink>
+
+                <div id="TopMenu" class=" md:block hidden pt-3">
+                    <ul class="
+                    flex 
+                    items-center 
+                    justify-end
+                    text-sm
+                    text-[#333333]
+                ">
+                        <li @mouseenter="isAccountMenu = true" @mouseleave="isAccountMenu = false"
+                            class="relative flex items-center px-2.5 hover:text-[#0C6539] h-full cursor-pointer"
+                            :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]' : 'border border-[#FAFAFA]'">
+                            <Icon name="ph:user-light" size="32" class="text-[#0C6539]" />
+                            <span class="ml-2">Account</span> <!-- Add margin-left to space the text from the icon -->
+
+                            <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+
+                            <div id="AccountMenu" v-if="isAccountMenu"
+                                class="absolute bg-white w-[200px] text-[#333333] z-40 top-[38px] -left-[0px] border-x border-b">
+                                <div v-if="!user">
+                                    <div class="text-semibold text-[15px] my-4 px-3">Welcome to Baybani!</div>
+                                    <div class="flex items-center gap-1 px-3 mb-3">
+                                        <NuxtLink to="/login"
+                                            class="bg-[#0C6539] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2">
+                                            Login
+                                        </NuxtLink>
+
+                                    </div>
+                                </div>
+
+
+                                <div class="border-b" />
+                                <ul class="bg-white ">
+                                    <li @click="navigateTo('/orders')"
+                                        class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">
+                                        My Orders
+                                    </li>
+                                    <li v-if="user" @click="client.auth.signOut()"
+                                        class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">
+                                        Sign out
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
 
                 <button @click="userStore.isMenuOverlay = true"
                     class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200">
